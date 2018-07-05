b7a50a04a2af43bebb9bd5c66a82bee74a114e55
prospero78su <prospero.78.su@gmail.com>
2018-07-05 19:59:05 +0200

    058 Рефакторинг: переименование метода

--- пакКомпилер/модКомпилер.py
+++ пакКомпилер/модКомпилер.py
@@ -1,24 +1,25 @@
+# coding: utf8
 """
 Модуль "Компилер".
 Содержит в себе все составляющие для компилятора Оберона
 """
-
-from .пакДиагностика.модКонсоль import тКонсоль
-from .пакДиагностика import тОшибка
-from .пакИсходник import тИсходник
-from .пакРесурс import тРесурс
-from .пакГуи import тГуи
+if True:
+	from .пакДиагностика.модКонсоль import тКонсоль
+	from .пакДиагностика import тОшибка
+	from .пакИсходник import тИсходник
+	from .пакРесурс import тРесурс
+	from .пакГуи import тГуи
 
 class тКомпилер:
-   def __init__(self):
-      self.рес = тРесурс(self)
-      self.конс = тКонсоль(self)
-      self.ошибка = тОшибка(self)
-      self.исх = тИсходник(self, "Hello.o7")
-      self.гуи = тГуи(self)
+	def __init__(self):
+		self.рес = тРесурс(self)
+		self.конс = тКонсоль(self)
+		self.ошибка = тОшибка(self)
+		self.исх = тИсходник(self, "Hello.o7")
+		self.гуи = тГуи(self)
 
 
-   def Пуск(self):
-      self.конс.Печать(self.рес.Компиляция)
-      self.исх.Обработать()
-      self.гуи.Пуск()
+	def Пуск(self):
+		self.конс.Печать(self.рес.Компиляция)
+		self.исх.Обработать()
+		self.гуи.Пуск()
