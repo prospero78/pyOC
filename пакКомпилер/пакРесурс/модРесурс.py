f0703fe8cf8d26f275ab5fb3d1fea67d33bcd7f4
prospero78su <prospero.78.su@gmail.com>
2018-07-05 20:36:45 +0200

    060 Рефакторинг, форматирование

--- пакКомпилер/пакРесурс/модРесурс.py
+++ пакКомпилер/пакРесурс/модРесурс.py
@@ -1,35 +1,36 @@
+# coding: utf8
 """
 Содержит класс ресурсов.
 """
 
 class тРесурс:
-   def __init__(self, root):
-      self.__root = root
-      self.app = {}
-      self.winMain = {}
-      self.Шапка()
-      self.Язык_Уст()
-   
-   def Шапка(self):
-      self.app['date'] = "2018-02-26"
-      self.app['time'] = "16:10"
-      self.app['build'] = "Build"
-      self.app['build_num'] = "054"
-      
-   def Язык_Уст(self, lang = "ru"):
-      if True:
-         self.app['name'] = 'Oberon-07 Compiler'
-         self.winMain['log'] = 'Log'
-         self.winMain['mnuFile'] = 'File'
-         self.winMain['mnuExit'] = 'Exit'
-         self.winMain['mnuOpen'] = 'Open...'
-         
-      if lang == "ru":
-         self.Компиляция = "Компиляция"
-         self.app['name'] = 'Компилятор Oberon-07'
-         self.app['name1'] = 'Oberon-07'
-         self.app['build'] = "Cборка"
-         self.winMain['log'] = 'Лог'
-         self.winMain['mnuFile'] = 'Файл'
-         self.winMain['mnuExit'] = 'Выход'
-         self.winMain['mnuOpen'] = 'Отрыть...'
+	def __init__(сам, корень):
+		сам.__корень = корень
+		сам.app     = {}
+		сам.winMain = {}
+		сам.Шапка_Уст()
+		сам.Язык_Уст()
+	
+	def Шапка_Уст(сам):
+		сам.app['date']  = "2018-07-05"
+		сам.app['time']  = "20:25"
+		сам.app['build'] = "Build"
+		сам.app['build_num'] = "059"
+		
+	def Язык_Уст(сам, lang = "ru"):
+		if True:
+			сам.app['name'] = 'Oberon-07 Compiler'
+			сам.winMain['log'] = 'Log'
+			сам.winMain['mnuFile'] = 'File'
+			сам.winMain['mnuExit'] = 'Exit'
+			сам.winMain['mnuOpen'] = 'Open...'
+			
+		if lang == "ru":
+			сам.Компиляция = "Компиляция"
+			сам.app['name']  = 'Компилятор Oberon-07'
+			сам.app['name1'] = 'Oberon-07'
+			сам.app['build'] = "Cборка"
+			сам.winMain['log'] = 'Лог'
+			сам.winMain['mnuFile'] = 'Файл'
+			сам.winMain['mnuExit'] = 'Выход'
+			сам.winMain['mnuOpen'] = 'Отрыть...'
