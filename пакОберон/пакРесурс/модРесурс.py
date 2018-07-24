6da6182eb4c0bc97eccb12fc8a5db850d1c3e9ce
prospero78su <prospero.78.su@gmail.com>
2018-07-24 16:02:57 +0200

    741 Исправление номера сборки

--- пакОберон/пакРесурс/модРесурс.py
+++ пакОберон/пакРесурс/модРесурс.py
@@ -12,10 +12,10 @@ class тРесурс:
 		сам.Язык_Уст()
 
 	def Шапка_Уст(сам):
-		сам.app['date']  = "2018-07-22"
-		сам.app['time']  = "19:36"
+		сам.app['date']  = "2018-07-24"
+		сам.app['time']  = "16:02"
 		сам.app['build'] = "Build"
-		сам.app['build_num'] = "691"
+		сам.app['build_num'] = "741"
 
 	def Язык_Уст(сам, lang = "ru"):
 		if True:
