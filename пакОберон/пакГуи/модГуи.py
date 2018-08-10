bb7857f8d4b9364d3c1b4f144c7855302514e2c0
prospero78su <prospero.78.su@gmail.com>
2018-08-10 21:06:24 +0200

    888 Добавление типизации для статического анализа

--- пакОберон/пакГуи/модГуи.py
+++ пакОберон/пакГуи/модГуи.py
@@ -5,8 +5,8 @@
 from .пакОкнГлав import тОкнГлав
 
 class тГуи:
-   def __init__(self, root):
+   def __init__(self, root)->None:
       self.окнГлав = тОкнГлав(root)
-   
-   def Пуск(self):
+
+   def Пуск(self)->None:
       self.окнГлав.Пуск()
