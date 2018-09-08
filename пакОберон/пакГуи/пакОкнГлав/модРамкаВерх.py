99431d9c30d5f5b828e00e0a80c0fff41808785b
prospero78su <prospero.78.su@gmail.com>
2018-09-08 13:56:44 +0200

    910 Добавление действий на события

--- пакОберон/пакГуи/пакОкнГлав/модРамкаВерх.py
+++ пакОберон/пакГуи/пакОкнГлав/модРамкаВерх.py
@@ -5,19 +5,32 @@
 from tkinter import Frame as тРамка, Menubutton as тКнпМну, Menu as тМеню
 
 class тРамкаВерх(тРамка):
-   def __init__(self, root, master):
-      self.__root = root
-      тРамка.__init__(self, master, relief='sunken', border=1)
-      self.pack(side="top", expand=True, fill='x')
+	def __init__(сам, root, предок):
+		сам.__root = root
+		сам.__предок = предок
+		тРамка.__init__(сам, предок, relief='sunken', border=1)
+		сам.pack(side="top", expand=True, fill='x')
 
 
-      self.кнпФайл = тКнпМну(self, text=root.рес.winMain['mnuFile'], relief='raised')
-      self.кнпФайл.pack(side='left')
+		сам.кнпФайл = тКнпМну(сам, text=root.рес.winMain['mnuFile'], relief='raised')
+		сам.кнпФайл.pack(side='left')
 
-      self.мнуФайл = тМеню(self.кнпФайл, tearoff = 0)
-      self.мнуФайл.add_command(label=root.рес.winMain['mnuOpen'], command = root.дисп.Файл_Открыть)
-      self.мнуФайл.add_separator()
-      self.мнуФайл.add_command(label=root.рес.winMain['mnuExit'])
+		сам.мнуФайл = тМеню(сам.кнпФайл, tearoff = 0)
+		сам.мнуФайл.add_command(label=root.рес.winMain['mnuOpen'], command = сам.Файл_Открыть)
+		сам.мнуФайл.add_separator()
+		сам.мнуФайл.add_command(label=root.рес.winMain['mnuExit'], command = root.дисп.Приложение_Закрыть)
 
+		сам.кнпФайл['menu'] = сам.мнуФайл
 
-      self.кнпФайл['menu'] = self.мнуФайл
+		сам.кнпАнализ = тКнпМну(сам, text=root.рес.winMain['mnuAnaliz'], relief='raised')
+		сам.кнпАнализ.pack(side='left')
+
+	def Файл_Открыть(сам, событие=None):
+		from tkinter import filedialog as fd
+		file_name = fd.askopenfilename(filetypes=(("Оберон файлы", "*.o7"),
+													 ("Текстовые файлы", "*.txt;*.me"),
+																("Все файлы", "*.*") ))
+		f = open(file_name, 'r', encoding='utf-8')
+		сИсх = f.read()
+		f.close()
+		сам.__root.дисп.Исх_Открыть(сИсх)
