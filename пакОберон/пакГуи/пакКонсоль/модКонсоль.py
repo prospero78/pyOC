35bcc1ce0880eecf68bf3bbb25ba6fdb97c9a155
prospero78su <prospero.78.su@gmail.com>
2018-09-13 07:35:29 +0200

    941 Добавление вывода в графическую консоль различной информации

--- пакОберон/пакГуи/пакКонсоль/модКонсоль.py
+++ пакОберон/пакГуи/пакКонсоль/модКонсоль.py
@@ -3,6 +3,7 @@
 Предоставляет тип консоли для служебной и отладочной печати.
 """
 
+import sys
 from tkinter import LabelFrame as тРамкаНадпись, Text as тТекст,\
 		Scrollbar as тПолзунок
 
@@ -27,17 +28,50 @@ class тКонсоль(тРамкаНадпись):
 		# вторая привязка
 		сам.редЛог['yscrollcommand'] = ползунок.set
 
-		сам.редЛог.tag_config('_normal_', font=("Consolas", 11, "normal"), foreground="#FFF", \
-				background="#000")
+		сам.редЛог.tag_config('_normal_', font=("Consolas", 11, "normal"), \
+				foreground="#FFF", background="#000")
 
 		сам.редЛог.tag_config('_head_', font=("Consolas", 11, "bold"), foreground="#FFF", \
 				background="#00F")
 
-	def Печать(сам, пСообщ:str)->None:
-		if type(пСообщ) == str:
-			сам.редЛог.insert("end", пСообщ+"\n", "_normal_")
+		сам.редЛог.tag_config('_source_', font=("Consolas", 11, ), foreground="#0F0", \
+				background="#000")
+
+		сам.редЛог.tag_config('_error_', font=("Consolas", 11, ), foreground="FB0", \
+				background="#F00")
+
+	def Проверить(сам, пбУсл:bool, пСообщ:str)->None:
+		if type(пбУсл) != bool:
+			сам.Ошибка("тКонсоль.Проверить(): пбУсл должен быть bool, type="+ \
+					str(type(пбУсл)))
+			if type(пОшибка) != str:
+				сам.Ошибка("тКонсоль.Проверить(): пСообщ должен быть str, type="+ \
+						str(type(пСообщ)))
+		if not пбУсл:
+			сам.Ошибка(пСообщ)
+
+	def Ошибка(сам, пОшибка:str)->None:
+		if type(пОшибка) == str:
+			сам.редЛог.insert("end", пОшибка+, "_error_")
+			сам.редЛог.insert('end', "\n", "_normal_")
 		else:
 			сам.Ошибка("тКонсоль.Печать(): пСообщ должен быть str, type="+str(type(пСообщ)))
+		sys.exit()
+
+	def Печать(сам, пСообщ:str)->None:
+		if type(пСообщ) == str:
+			сам.редЛог.insert("end", пСообщ+, "_normal_")
+			сам.редЛог.insert('end', "\n", "_normal_")
+		else:
+			сам.Ошибка("тКонсоль.Печать(): пСообщ должен быть str, type="+str(type(пСообщ)))
+
+	def Исх_Печать(сам, пСообщ:str)->None:
+		if if type(пСообщ) == str:
+			сам.редЛог.insert("end", пСообщ+"\n", "_source_")
+			сам.редЛог.insert('end', "\n", "_normal_")
+		else:
+			сам.Ошибка("тКонсоль.Исх_Печать(): пСообщ должен быть str, type="+\
+					str(type(пСообщ)))
 
 	def Шапка_Печать(сам)->None:
 		рес = сам.__оберон.рес
